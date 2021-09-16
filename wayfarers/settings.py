28f3b0f5cd7c8116df36a2e2c128593c36211426
elainebroche-dev <elainebroche@gmail.com>
2021-09-16 11:30:31 +0000

    Fix misspelling of ALLOWED_HOST value

--- wayfarers/settings.py
+++ wayfarers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['p4-wayfarers.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pf4-wayfarers.herokuapp.com', 'localhost']
 
 
 # Application definition
