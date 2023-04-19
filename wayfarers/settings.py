9f2b7ff8948b8a3aa00aefe3c953cd1ec54defdd
elainebroche-dev <elainebrochedev@gmail.com>
2023-04-19 17:56:31 +0000

    set debug to false

--- wayfarers/settings.py
+++ wayfarers/settings.py
@@ -30,10 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if development:
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
